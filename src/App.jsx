769a6141ed19914edfb354de0ee2e0e67b0852ac
Mateen Mirani <mateenmirani125@gmail.com>
2022-03-06 09:07:52 +0500

    connected to firebase database

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/UI/Shop/Products";
@@ -7,7 +7,14 @@ import { useSelector } from "react-redux";
 
 function App() {
   const uiState = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
 
+  useEffect(() => {
+    fetch("https://cart-data-26c5d-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
   return (
     <Layout>
       {uiState && <Cart />}
